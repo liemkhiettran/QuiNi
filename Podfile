4f00e98989955aab6252e1d5563b376eb11088d9
liem-khiet tran <liemkhiet.tran@gmail.com>
2018-01-14 08:11:01 +0400

    initial commit

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'QuiNi' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for QuiNi
+
+  target 'QuiNiTests' do
+    inherit! :search_paths
+    # Pods for testing
+
+    pod 'Quick'
+    pod 'Nimble'
+  end
+
+end
---
+++ QuiNiTests/QuiNiTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuiNiTests.swift
+//  QuiNiTests
+//
+//  Created by Liemkhiet Tran on 1/14/18.
+//  Copyright © 2018 liem-khiet tran. All rights reserved.
+//
+
+import XCTest
+@testable import QuiNi
+
+class QuiNiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
